364f6c37d3fc2abcea3e1870d540e7e9ce88c10b
German <swps.aka001@mail.ru>
2026-05-21 21:02:50 +0300

    add exec grpc server with graceful shutdown

---
+++ services/user/cmd/server/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/north-fy/talker/services/user/internal/app"
+	"github.com/north-fy/talker/services/user/internal/config"
+	"go.uber.org/zap"
+)
+
+func main() {
+	cfg := config.Config{}
+	if err := cfg.Load(); err != nil {
+		panic(err)
+	}
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+
+	defer logger.Sync()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	server := app.New(ctx, logger, cfg.GRPCCfg.Port, cfg.PostgresCfg)
+	server.GRPCSrv.MustRun()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, os.Kill)
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			cancel()
+			server.GRPCSrv.Stop()
+			logger.Error("server stopped", zap.Error(ctx.Err()))
+		case <-sigChan:
+			cancel()
+			server.GRPCSrv.Stop()
+			logger.Info("server stopped by sigChan")
+		}
+	}()
+}
